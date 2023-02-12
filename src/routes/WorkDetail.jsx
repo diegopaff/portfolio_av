9a0cbd7234b9b1ece5d046dd6b1eb4559a132eb7
paff <diego.curutchet89@gmail.com>
2023-02-12 11:40:19 -0300

    New stylize buttons for prev and next video in WorkDetails

--- src/routes/WorkDetail.jsx
+++ src/routes/WorkDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { videos } from '../constants'
 import { Link } from 'react-router-dom'
-import { GrLinkNext , GrLinkPrevious} from 'react-icons/gr'
+import { AiOutlineLeft , AiOutlineRight } from 'react-icons/ai'
 
 const WorkDetail = () => {
 
@@ -27,19 +27,19 @@ const WorkDetail = () => {
         <div className='w-full max-w-[1600px]'>
             <iframe src={video.video} className="w-full aspect-video" allowFullScreen></iframe>
         </div>
-        <div className='flex flex-col font-poppins w-full max-w-[1600px]'>
+        <div className='flex flex-col font-poppins w-full max-w-[1600px] gap-5'>
             <div className="flex items-baseline font-poppins gap-5">
                 <h3 className='font-semibold text-3xl text-black dark:text-white'>{video.title}</h3>
-                <p className='font-light text-sm text-gray-600 dark:text-gray-200 '> {`( ${video.rol} )`}</p>
+                <p className='font-light text-sm text-gray-600 dark:text-gray-300 '> {`( ${video.rol} )`}</p>
             </div>
             <p className='font-regular text-base text-black dark:text-white'>{video.description}</p>
         </div>
-        <div className='flex w-full max-w-[1600px] mx-auto justify-between font-poppins'>
+        <div className='flex w-full max-w-[1200px] mx-auto justify-between font-poppins my-5'>
            <Link className='flex text-black dark:text-white gap-2 items-center' to={`/work/${videos[indexPrevVideo].value}`}>
-                <GrLinkPrevious className='text-3xl text-black dark:text-white '/> <span>Video anterior</span>
+                <AiOutlineLeft className='text-3xl text-black dark:text-white '/> <span>Video anterior</span>
             </Link>
             <Link className='flex text-black dark:text-white gap-2 items-center' to={`/work/${videos[indexNextVideo].value}`}>
-                <span>Próximo Video</span> <GrLinkNext className='text-3xl text-white' />  
+                <span>Próximo Video</span> <AiOutlineRight className='text-3xl text-black dark:text-white' />  
             </Link>
             
         </div>
