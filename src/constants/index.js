27cb38bb61ce918a28399897115c786e367e410b
diegopaff <difercuru@gmail.com>
2024-01-10 20:59:32 -0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,82 +1,118 @@
 export const videos = [
-    {
-        id: '1',
-        title: 'Genexus en Pagnifique',
-        category: 'corporativo',
-        value: 'Genexus',
-        description: 'La empresa Genexus necesitaba un video de caso éxito donde muestra el departamento IT de Pagnifique utilizando su tecnología en la empresa y explicando como se beneficia particularmente con la misma. ',
-        /* thumbnail: '../../public/pagnifique.jpg', */
-        thumbnail: 'https://i.ibb.co/9wMj1HR/pagnifique.jpg',
-        rol: 'Edición',
-        video: 'https://drive.google.com/file/d/1cNA2yevgZapjv-vDElOf4CCKvSH1wYJV/preview',
-    },
-    {
-        id: '2',
-        title: 'Sistema panelizado Montfrio',
-        category: 'corporativo',
-        value: 'Montfrio',
-        description: 'Este video es parte de una collección de 10 videos donde se explica el paso a paso de la construcción de una vivienda utilizando el sistema SPM (Sistema Panelizado Montfrio).',
-        /* thumbnail: '../../public/Montfrio.jpg', */
-        thumbnail: 'https://i.ibb.co/hZFvRbh/Montfrio.jpg',
-        rol: 'Edición y filmación',
-        video: 'https://drive.google.com/file/d/18UCjZfoRzIq9_BsWbBGA1Cr-DMfISNac/preview',
-    },
-    {
-        id: '3',
-        title: 'Instructor de Golf',
-        category: 'socialMedia',
-        value: 'CaseyGolf',
-        description: 'Edición de video para redes sociales.',
-        /* thumbnail: '../../public/caseyGolf.jpg', */
-        thumbnail: 'https://i.ibb.co/MgBKLKk/casey-Golf.jpg',
-        rol: 'Edición',
-        video: 'https://drive.google.com/file/d/1CG4NdVDPwfhb-fO4D2j2QQmdiqkVIOpV/preview',
-    },
-    {
-        id: '4',
-        title: 'Golf tips by Casey',
-        category: 'socialMedia',
-        value: 'CaseyGolfCoach',
-        description: 'Edición de video para redes sociales.',
-        /* thumbnail: '../../public/caseyGolfCoach.jpg', */
-        thumbnail: 'https://i.ibb.co/hsLW6PB/casey-Golf-Coach.jpg',
-        rol: 'Edición',
-        video: 'https://drive.google.com/file/d/1eSbeo8JLYvojHox5wl5C1HPfV_E42Tx_/preview',
-    },    
-    {
-        id: '5',
-        title: 'Energy Drink Suplement',
-        category: 'socialMedia',
-        value: 'DanceSmoothie',
-        description: 'Edición de video para redes sociales.',
-        /* thumbnail: '../../public/EnergyDrinkDance.jpg', */
-        thumbnail: 'https://i.ibb.co/mXBTjHp/Energy-Drink-Dance.jpg',
-        rol: 'Edición',
-        video: 'https://drive.google.com/file/d/1lqpz0CkMDXakVuztTGZJZCPIIpm2AsKy/preview',
-    },
-    {
-        id: 'Shambies1',
-        title: 'Shambies en Gallager',
-        category: 'socialMedia',
-        value: 'ShambiesGallagher',
-        description: 'Cobertura del show y edición de video para reel de instagram de la banda.',
-        /* thumbnail: '../../public/EnergyDrinkDance.jpg', */
-        thumbnail: 'https://i.ibb.co/XtRpky2/Shambies-en-gallager.png',
-        rol: 'Edición y filmación',
-        video: 'https://drive.google.com/file/d/1Jygh--GEic3HkKW29q_t2gtEBdK0lNUo/preview',
-    },
-    {
-        id: '6',
-        title: 'Ride Pico Delivery',
-        category: 'socialMedia',
-        value: 'ridePico',
-        description: 'Edición de video para redes sociales.',
-        /* thumbnail: '../../public/ridePico.jpg', */
-        thumbnail: 'https://i.ibb.co/1K8GXvm/ridePico.jpg',
-        rol: 'Edición',
-        video: 'https://drive.google.com/file/d/1QDYVNjupCHeJq8Ezk-UT-hMtBwvR2__j/preview',
-    },
- /*    {
+  {
+    id: "12",
+    title: "Video institucional GSK",
+    category: "corporativo",
+    value: "Vipermed",
+    description:
+      "Edición de video institucional para uso interno entre vendedores de la empresa.",
+    /* thumbnail: '../../public/traslado.jpg', */
+    thumbnail: "https://i.ibb.co/5c817fG/GSk.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1Mngj_egB3SUxHCkDKXWW2yH5XzbV3RDa/preview",
+  },
+  {
+    id: "0",
+    title: "Tour casa de playa",
+    category: "corporativo",
+    value: "Ramsa",
+    description:
+      "Dueños de la casa necesitaban un video para mostrarla con la intención de alquilarla. ",
+    /* thumbnail: '../../public/pagnifique.jpg', */
+    thumbnail: "https://i.ibb.co/bv2kKV1/chihuahua.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1K7Ca7Iny5yUZMYP7RaZdHZXYvUzumWZM/preview",
+  },
+  {
+    id: "1",
+    title: "Genexus en Pagnifique",
+    category: "corporativo",
+    value: "Genexus",
+    description:
+      "La empresa Genexus necesitaba un video de caso éxito donde muestra el departamento IT de Pagnifique utilizando su tecnología en la empresa y explicando como se beneficia particularmente con la misma. ",
+    /* thumbnail: '../../public/pagnifique.jpg', */
+    thumbnail: "https://i.ibb.co/9wMj1HR/pagnifique.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1cNA2yevgZapjv-vDElOf4CCKvSH1wYJV/preview",
+  },
+  {
+    id: "2",
+    title: "Sistema panelizado Montfrio",
+    category: "corporativo",
+    value: "Montfrio",
+    description:
+      "Este video es parte de una collección de 10 videos donde se explica el paso a paso de la construcción de una vivienda utilizando el sistema SPM (Sistema Panelizado Montfrio).",
+    /* thumbnail: '../../public/Montfrio.jpg', */
+    thumbnail: "https://i.ibb.co/hZFvRbh/Montfrio.jpg",
+    rol: "Edición y filmación",
+    video:
+      "https://drive.google.com/file/d/18UCjZfoRzIq9_BsWbBGA1Cr-DMfISNac/preview",
+  },
+  {
+    id: "3",
+    title: "Instructor de Golf",
+    category: "socialMedia",
+    value: "CaseyGolf",
+    description: "Edición de video para redes sociales.",
+    /* thumbnail: '../../public/caseyGolf.jpg', */
+    thumbnail: "https://i.ibb.co/MgBKLKk/casey-Golf.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1CG4NdVDPwfhb-fO4D2j2QQmdiqkVIOpV/preview",
+  },
+  {
+    id: "4",
+    title: "Golf tips by Casey",
+    category: "socialMedia",
+    value: "CaseyGolfCoach",
+    description: "Edición de video para redes sociales.",
+    /* thumbnail: '../../public/caseyGolfCoach.jpg', */
+    thumbnail: "https://i.ibb.co/hsLW6PB/casey-Golf-Coach.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1eSbeo8JLYvojHox5wl5C1HPfV_E42Tx_/preview",
+  },
+  {
+    id: "5",
+    title: "Energy Drink Suplement",
+    category: "socialMedia",
+    value: "DanceSmoothie",
+    description: "Edición de video para redes sociales.",
+    /* thumbnail: '../../public/EnergyDrinkDance.jpg', */
+    thumbnail: "https://i.ibb.co/mXBTjHp/Energy-Drink-Dance.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1lqpz0CkMDXakVuztTGZJZCPIIpm2AsKy/preview",
+  },
+  {
+    id: "Shambies1",
+    title: "Shambies en Gallager",
+    category: "socialMedia",
+    value: "ShambiesGallagher",
+    description:
+      "Cobertura del show y edición de video para reel de instagram de la banda.",
+    /* thumbnail: '../../public/EnergyDrinkDance.jpg', */
+    thumbnail: "https://i.ibb.co/XtRpky2/Shambies-en-gallager.png",
+    rol: "Edición y filmación",
+    video:
+      "https://drive.google.com/file/d/1Jygh--GEic3HkKW29q_t2gtEBdK0lNUo/preview",
+  },
+  {
+    id: "6",
+    title: "Ride Pico Delivery",
+    category: "socialMedia",
+    value: "ridePico",
+    description: "Edición de video para redes sociales.",
+    /* thumbnail: '../../public/ridePico.jpg', */
+    thumbnail: "https://i.ibb.co/1K8GXvm/ridePico.jpg",
+    rol: "Edición",
+    video:
+      "https://drive.google.com/file/d/1QDYVNjupCHeJq8Ezk-UT-hMtBwvR2__j/preview",
+  },
+  /*    {
         id: '4',
         title: 'Hostel Vida Futbol',
         category: 'corporativo',
@@ -85,111 +121,114 @@ export const videos = [
         thumbnail: '../../public/vidafutbol.jpg',
         video: 'https://drive.google.com/file/d/1gz1-tSoK3Mjdiw8OsC0JP4jt2nkVxaDO/preview',
     }, */
-    {
-        id: '7',
-        title: 'UX Divers fiesta fin de año',
-        category: 'corporativo',
-        value: 'UXdivers',
-        description: 'Video registro de la fiesta de fin de año de la empresa de Software UXdivers con el objetivo de usarlo en Linkedin y de forma interna',
-        /* thumbnail: '../../public/UXdivers.jpg', */
-        thumbnail: 'https://i.ibb.co/NtrgpwD/UXdivers.jpg',
-        rol: 'Edición y filmación',
-        video: 'https://drive.google.com/file/d/1YAdh3L2psz591va46PDQwHo8RA6gSZw8/preview',
-    },
-    {
-        id: '8',
-        title: 'Parado en un Pie (Videoclip)',
-        category: 'videoclip',
-        value: 'ParadoEnUnPie',
-        description: 'Filmación y edición de videoclip para la banda Hijos de la Marea.',
-        /* thumbnail: '../../public/paradoEnUnPie.jpg', */
-        thumbnail: 'https://i.ibb.co/ZSMkv0R/parado-En-Un-Pie.jpg',
-        rol: 'Edición y filmación',
-        video: 'https://drive.google.com/file/d/1TWEzmWkDpJWp_HTXIi3RZgQL8JYF7u8B/preview',
-    },
-    {
-        id: '9',
-        title: 'Navegando (Videoclip)',
-        category: 'videoclip',
-        value: 'Navegando',
-        description: 'Filmación y edición de videoclip para la banda Hijos de la Marea.',
-        /* thumbnail: '../../public/navegando.jpg', */
-        thumbnail: 'https://i.ibb.co/zGrrGVZ/navegando.jpg',
-        rol: 'Edición y filmación',
-        video: 'https://www.youtube.com/embed/FzRRQ9ZkVlA',
-    },
-    {
-        id: '10',
-        title: 'Aire del bueno (Videoclip)',
-        category: 'videoclip',
-        value: 'AireDelBueno',
-        description: 'Edición de videoclip para la banda Hijos de la Marea.',
-        /* thumbnail: '../../public/aireDelBueno.jpg', */
-        thumbnail: 'https://i.ibb.co/2tBCNLc/aire-Del-Bueno.jpg',
-        rol: 'Edición',
-        video: 'https://www.youtube.com/embed/FLTnv5DsAzU',
-    },
-    {
-        id: '11',
-        title: 'Traslado de Casa Pre-fabricada',
-        category: 'corporativo',
-        value: 'TrasladoCasa',
-        description: 'Filmación y Edición de video registro del proceso de traslado de una casa prefabricada.',
-        /* thumbnail: '../../public/traslado.jpg', */
-        thumbnail: 'https://i.ibb.co/6sTBySm/traslado.jpg',
-        rol: 'Edición y filmación',
-        video: 'https://drive.google.com/file/d/10W_vYEh_LqVE6PRzu4qe1kMJ_8wv4kla/preview',
-    },
-   
-      
-   
+  {
+    id: "7",
+    title: "UX Divers fiesta fin de año",
+    category: "corporativo",
+    value: "UXdivers",
+    description:
+      "Video registro de la fiesta de fin de año de la empresa de Software UXdivers con el objetivo de usarlo en Linkedin y de forma interna",
+    /* thumbnail: '../../public/UXdivers.jpg', */
+    thumbnail: "https://i.ibb.co/NtrgpwD/UXdivers.jpg",
+    rol: "Edición y filmación",
+    video:
+      "https://drive.google.com/file/d/1YAdh3L2psz591va46PDQwHo8RA6gSZw8/preview",
+  },
+  {
+    id: "8",
+    title: "Parado en un Pie (Videoclip)",
+    category: "videoclip",
+    value: "ParadoEnUnPie",
+    description:
+      "Filmación y edición de videoclip para la banda Hijos de la Marea.",
+    /* thumbnail: '../../public/paradoEnUnPie.jpg', */
+    thumbnail: "https://i.ibb.co/ZSMkv0R/parado-En-Un-Pie.jpg",
+    rol: "Edición y filmación",
+    video:
+      "https://drive.google.com/file/d/1TWEzmWkDpJWp_HTXIi3RZgQL8JYF7u8B/preview",
+  },
+  {
+    id: "9",
+    title: "Navegando (Videoclip)",
+    category: "videoclip",
+    value: "Navegando",
+    description:
+      "Filmación y edición de videoclip para la banda Hijos de la Marea.",
+    /* thumbnail: '../../public/navegando.jpg', */
+    thumbnail: "https://i.ibb.co/zGrrGVZ/navegando.jpg",
+    rol: "Edición y filmación",
+    video: "https://www.youtube.com/embed/FzRRQ9ZkVlA",
+  },
+  {
+    id: "10",
+    title: "Aire del bueno (Videoclip)",
+    category: "videoclip",
+    value: "AireDelBueno",
+    description: "Edición de videoclip para la banda Hijos de la Marea.",
+    /* thumbnail: '../../public/aireDelBueno.jpg', */
+    thumbnail: "https://i.ibb.co/2tBCNLc/aire-Del-Bueno.jpg",
+    rol: "Edición",
+    video: "https://www.youtube.com/embed/FLTnv5DsAzU",
+  },
+  {
+    id: "11",
+    title: "Traslado de Casa Pre-fabricada",
+    category: "corporativo",
+    value: "TrasladoCasa",
+    description:
+      "Filmación y Edición de video registro del proceso de traslado de una casa prefabricada.",
+    /* thumbnail: '../../public/traslado.jpg', */
+    thumbnail: "https://i.ibb.co/6sTBySm/traslado.jpg",
+    rol: "Edición y filmación",
+    video:
+      "https://drive.google.com/file/d/10W_vYEh_LqVE6PRzu4qe1kMJ_8wv4kla/preview",
+  },
 ];
 
-export const navLinks = [
-    {
-        id: 'nav1',
-        title: 'Trabajo',
-        linkTo: '',
-    },
-    {
-        id: 'nav2',
-        title: 'Sobre Mi',
-        linkTo: 'about',
-    },
-    {
-        id: 'nav3',
-        title: 'Curriculum',
-        linkTo: 'curriculum',
-    },
-    {
-        id: 'nav4',
-        title: 'Contacto',
-        linkTo: 'contact',
-    },
+videos.map;
 
+export const navLinks = [
+  {
+    id: "nav1",
+    title: "Trabajo",
+    linkTo: "",
+  },
+  {
+    id: "nav2",
+    title: "Sobre Mi",
+    linkTo: "about",
+  },
+  {
+    id: "nav3",
+    title: "Curriculum",
+    linkTo: "curriculum",
+  },
+  {
+    id: "nav4",
+    title: "Contacto",
+    linkTo: "contact",
+  },
 ];
 
 export const videoTypes = [
-    {
-        id: 'videoTypeAll',
-        title: 'Todos',
-        value: 'all',
-    },
-    {
-        id: 'videoType1',
-        title: 'Corporativo',
-        value: 'corporativo',
-    },
-    {
-        id: 'videoType2',
-        title: 'Videoclip de Música',
-        value: 'videoclip',
-    },
-    {
-        id: 'videoType3',
-        title: 'Redes Sociales',
-        value: 'socialMedia',
-    },
-    
-
+  {
+    id: "videoTypeAll",
+    title: "Todos",
+    value: "all",
+  },
+  {
+    id: "videoType1",
+    title: "Corporativo",
+    value: "corporativo",
+  },
+  {
+    id: "videoType2",
+    title: "Videoclip de Música",
+    value: "videoclip",
+  },
+  {
+    id: "videoType3",
+    title: "Redes Sociales",
+    value: "socialMedia",
+  },
 ];
